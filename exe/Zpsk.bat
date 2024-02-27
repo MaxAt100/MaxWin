0628418201d6efc4f3382710a4ab351fd7bd13a3
Максим <159615225+MaxAt100@users.noreply.github.com>
2024-02-27 18:14:45 +0500

    Update Zpsk.bat

--- exe/Zpsk.bat
+++ exe/Zpsk.bat
@@ -1,3 +1,3 @@
 @echo off
-start C:\Program Files\MaxWin\exe\effect.exe
-start C:\Program Files\MaxWin\exe\sound.exe
+start C:\MaxWin\MaxWin-main\exe\effect.exe
+start C:\MaxWin\MaxWin-main\exe\sound.exe
